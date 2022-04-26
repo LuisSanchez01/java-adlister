1a51f268f0187b9feea1fb6733ed161be52f1c6d
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-04-26 11:39:08 -0500

    add HelloWorldServlet.java

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,22 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
+    @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
+
+        @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        ServletResponse response = null;
+            assert false;
+            response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello, World!</h1>");
     }
 }
