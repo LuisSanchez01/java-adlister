3b46a77e2fcfcef39eb028e33f85ab585d6d2623
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-04-28 15:20:09 -0500

    add Product.java

--- src/main/java/ProductsServlet.java
+++ src/main/java/ProductsServlet.java
@@ -1,4 +1,22 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-
-public class ProductsServlet extends HttpServlet {
-}
+import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.util.List;
+//
+//@WebServlet(name = "ProductsServlet", urlPatterns = "/Products")
+//public class ProductsServlet extends HttpServlet {
+//
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        Products productsDao = DaoFactory.getProductsDao();
+//        List<Product> products = productsDao.all();
+//
+//        request.setAttribute("products", products);
+//        RequestDispatcher view = request.getRequestDispatcher("/products.jsp");
+//        view.forward.doGet(request, response);
+//    }
+//}
