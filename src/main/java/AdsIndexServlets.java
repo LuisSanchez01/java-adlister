16d8a34cfa0cf6cb30cf1cb19a844be5fa49250c
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-05-02 10:41:42 -0500

    add changes to AdsIndexServlets.java

--- src/main/java/AdsIndexServlets.java
+++ src/main/java/AdsIndexServlets.java
@@ -9,6 +9,6 @@ import java.io.IOException;
 public class AdsIndexServlets extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      request.setAttribute("ads", DaoFactory);
+      request.setAttribute("ads", "DaoFactory");
     }
 }
